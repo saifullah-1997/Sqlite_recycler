7676c80a8d8cdac132234938db1bc7d34de11358
saifullah-1997 <60419056+saifullah-1997@users.noreply.github.com>
2020-04-22 10:48:49 +0500

    Initial commit

---
+++ app/src/main/java/com/demo/sqlitedababase/Recycler/Recyle.java
@@ -0,0 +1,13 @@
+package com.demo.sqlitedababase.Recycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recyle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyle);
+    }
+}
